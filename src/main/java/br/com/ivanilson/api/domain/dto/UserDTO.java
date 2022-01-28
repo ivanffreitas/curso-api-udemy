72da7bc2f03d1333ffde6698a0877b0583d9ac9e
ivanffreitas <ivanff.ti@gmail.com>
2022-01-28 10:13:21 -0300

    Endpoint create para user

--- src/main/java/br/com/ivanilson/api/domain/dto/UserDTO.java
+++ src/main/java/br/com/ivanilson/api/domain/dto/UserDTO.java
@@ -1,6 +1,6 @@
 package br.com.ivanilson.api.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +16,7 @@ public class UserDTO {
     private String name;
     private String email;
 
-    @JsonIgnore //não retornar o campo para o front
+    //@JsonIgnore //ignorar o campo
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) //Liberar para escrita, listagem não
     private String password;
 }
--- src/main/java/br/com/ivanilson/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/ivanilson/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.ivanilson.api.services.impl;
 
 import br.com.ivanilson.api.domain.User;
+import br.com.ivanilson.api.domain.dto.UserDTO;
 import br.com.ivanilson.api.repositories.UserRepository;
 import br.com.ivanilson.api.services.UserService;
 import br.com.ivanilson.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
@@ -24,5 +29,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class));
     }
 }
