4f7623560ec5358c449d336717f80a153dd4d36a
ivanffreitas <ivanff.ti@gmail.com>
2022-01-28 09:39:52 -0300

    Endpoint fidAll para user

--- src/main/java/br/com/ivanilson/api/services/UserService.java
+++ src/main/java/br/com/ivanilson/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.ivanilson.api.services;
 
 import br.com.ivanilson.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
