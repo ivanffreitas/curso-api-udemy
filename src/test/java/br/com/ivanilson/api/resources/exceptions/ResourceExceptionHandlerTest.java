f4d8eae2facc18db70890844337ffc95af164053
ivanffreitas <ivanff.ti@gmail.com>
2022-01-31 22:01:17 -0300

    Teste para manipulador de excecoes metodo object not found

--- src/test/java/br/com/ivanilson/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/ivanilson/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.ivanilson.api.resources.exceptions;
 
+import br.com.ivanilson.api.services.exceptions.DataIntegratyViolationException;
 import br.com.ivanilson.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +32,7 @@ class ResourceExceptionHandlerTest {
                 .objectNotFound(
                         new ObjectNotFoundException("Objeto não encontrado"),
                         new MockHttpServletRequest());
+
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
@@ -36,9 +40,22 @@ class ResourceExceptionHandlerTest {
         assertEquals(StandarError.class, response.getBody().getClass());
         assertEquals("Objeto não encontrado", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandarError> response = exceptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException("E-mail já cadastrado"),
+                        new MockHttpServletRequest());
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandarError.class, response.getBody().getClass());
+        assertEquals("E-mail já cadastrado", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
